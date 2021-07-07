083c2621f74ba26452d0d5743a30c223862caf45
Ruiz <46197155+ruiz94@users.noreply.github.com>
2021-07-07 14:34:24 -0500

    Cambios en tamaños del sitio

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {HashRouter, Route, Switch } from 'react-router-dom';
 
 import Entradas from '../containers/Entradas';
 import NotFound from '../containers/NotFound';
@@ -7,14 +7,14 @@ import DetallesEntrada from '../containers/DetallesEntrada';
 
 const Routes = () => {
   return ( 
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Entradas}/>
-        <Route exact path="/nueva-entrada" component={NuevaEntrada}/>
-        <Route exact path="/detalle-entrada/:id" component={DetallesEntrada}/>
-        <Route component={NotFound}/>
-      </Switch>
-    </Router>
+    <HashRouter>
+        <Switch>
+          <Route exact path="/" component={Entradas}/>
+          <Route exact path="/nueva-entrada" component={NuevaEntrada}/>
+          <Route exact path="/detalle-entrada/:id" component={DetallesEntrada}/>
+          <Route component={NotFound}/>
+        </Switch>
+    </HashRouter>
    );
 }
  
